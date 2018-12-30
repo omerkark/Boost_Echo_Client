d224aab31dd2e156ec04cf74669fe4ee48f89113
omerkark <omerkark@gmail.com>
2018-12-31 00:01:16 +0200

    echo

---
+++ include/readFromKeyBoard.h
@@ -0,0 +1,12 @@
+//
+// Created by omerkark@wincs.cs.bgu.ac.il on 12/30/18.
+//
+
+#ifndef BOOST_ECHO_CLIENT_READFROMKEYBOARD_H
+#define BOOST_ECHO_CLIENT_READFROMKEYBOARD_H
+
+#endif //BOOST_ECHO_CLIENT_READFROMKEYBOARD_H
+
+class readFromKeyBoard{
+    public:
+};
